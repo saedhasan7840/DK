78e61946b54f1d67525df694bc07e7050a00d4eb
AK Saed <165506282+saedhasan7840@users.noreply.github.com>
2024-08-15 17:14:31 +0600

    Update DK.py

--- DK.py
+++ DK.py
@@ -37,7 +37,6 @@ user = []
 #__________________[ DATE ]__________________#
 dic = {'1':'JANUARY','2':'FEBRUARY','3':'MARCH','4':'APRIL','5':'MAY','6':'JUNE','7':'JULY','8':'AUGUST','9':'SEPTEMBER','10':'OCTOBER','11':'NOVEMBER','12':'DECEMBER'}
 dic2 = {'01':'JANUARY','02':'FEBRUARY','03':'MARCH','04':'APRIL','05':'MAY','06':'JUNE','07':'JULY','08':'AUGUST','09':'SEPTEMBER','10':'OCTOBER','11':'NOVEMBER','12':'DECEMBER'}
-tgl = datetime.datetime.now(30).day
 bln = dic[(str(datetime.datetime.now(1).month))]
 thn = datetime.datetime.now(1).year
 date ='\x1b[1;97m'+str(tgl)+'\x1b[1;97m/\x1b[1;97m'+str(bln)+'\x1b[1;97m/\x1b[1;97m'+str(thn)
